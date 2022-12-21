4e558c75e90e2f1e6fdfea9abd5684bf05362240
DJ-Andhi <121202450+DJ-Andhi@users.noreply.github.com>
2022-12-22 04:28:19 +0530

    Delete httpd.sh

--- httpd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-wget -P /var/www/html https://demowebbucket12.s3.amazonaws.com/iPortfolio.zip
-sudo unzip /var/www/html/iPortfolio.zip
-sudo mv iPortfolio/* /var/www/html/
